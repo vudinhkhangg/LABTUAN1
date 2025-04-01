7ae3777c6bc3b968f38530602fbf3e3c520fd94e
vudinhkhangg <1250080081@sv.hcmunre.edu.vn>
2025-04-01 17:05:00 +0700

    bai1.java

---
+++ bai1.java
@@ -0,0 +1,8 @@
+class bai1 {
+    public static void main(String[] args) {
+        String a ="Vu Dinh Khang";
+        String b ="15/1/2005";
+        String c ="1250080081";
+        System.out.println("toi ten la "+a+" sinh ngay "+b+" co ma so sinh vien la "+c);
+    }
+}
