e16d096195e4d4ba42027704cd69cb662fd98ec6
vudinhkhangg <1250080081@sv.hcmunre.edu.vn>
2025-04-01 17:06:53 +0700

    bai2.java

---
+++ bai2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class bai2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap chieu dai, chieu rong:");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println("dien tich hinh chu nhat la: "+a*b);
+    }
+}
