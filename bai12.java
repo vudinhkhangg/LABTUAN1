c91794867b5d5ef5477d81d65d13f601d14fb291
vudinhkhangg <1250080081@sv.hcmunre.edu.vn>
2025-04-01 17:15:42 +0700

    Update and rename bai13 to bai12.java

--- bai12.java
+++ bai12.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class bai13 {
+class bai12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("nhap mot so:");
